933fff5214aade3f799af33f33be40b4452a2bba
Gauthier Bastien <g.bastien@imio.be>
2019-11-18 17:29:08 +0100

    Begin to add @search_items endpoint

--- src/plonemeeting/restapi/services/item.py
+++ src/plonemeeting/restapi/services/item.py
@@ -4,10 +4,10 @@ from plone import api
 from plone.restapi.services.search.get import SearchGet
 
 
-class BaseSearchMeetingGet(SearchGet):
+class BaseSearchItemGet(SearchGet):
 
     def __init__(self, context, request):
-        super(BaseSearchMeetingGet, self).__init__(context, request)
+        super(BaseSearchItemGet, self).__init__(context, request)
         self.tool = api.portal.get_tool('portal_plonemeeting')
         config_id = self._config_id
         self.cfg = self.tool.get(config_id, None)
@@ -33,47 +33,20 @@ class BaseSearchMeetingGet(SearchGet):
     def _set_additional_query_params(self):
         ''' '''
         form = self.request.form
-        form['portal_type'] = self.cfg.getMeetingTypeName()
+        form['portal_type'] = self.cfg.getItemTypeName()
         form['metadata_fields'] = form.get('metadata_fields', []) + \
             self._additional_fields
         form['fullobjects'] = True
-        # avoid batch
-        form['b_size'] = 9999
 
     def reply(self):
         self._set_additional_query_params()
-        return super(BaseSearchMeetingGet, self).reply()
+        return super(BaseSearchItemGet, self).reply()
 
 
-class SearchMeetingsGet(BaseSearchMeetingGet):
+class SearchItemsGet(BaseSearchItemGet):
     ''' '''
 
     def _set_additional_query_params(self):
-        super(SearchMeetingsGet, self)._set_additional_query_params()
+        super(SearchItemsGet, self)._set_additional_query_params()
         form = self.request.form
         form['sort_on'] = form.get('sort_on', 'sortable_title')
-
-
-class SearchMeetingItemsGet(BaseSearchMeetingGet):
-
-    @property
-    def _meeting_uid(self):
-        if 'linkedMeetingUID' not in self.request.form:
-            raise Exception(
-                "The \"linkedMeetingUID\" parameter must be given"
-            )
-        return self.request.form.get('linkedMeetingUID')
-
-    @property
-    def _additional_fields(self):
-        ''' '''
-        fields = super(SearchMeetingItemsGet, self)._additional_fields
-        fields += ['itemNumber']
-        return fields
-
-    def _set_additional_query_params(self):
-        super(SearchMeetingItemsGet, self)._set_additional_query_params()
-        form = self.request.form
-        form['linkedMeetingUID'] = self._meeting_uid
-        form['portal_type'] = self.cfg.getItemTypeName()
-        form['sort_on'] = form.get('sort_on', 'getItemNumber')
