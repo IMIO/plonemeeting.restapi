b947fed1ae168574fcebc89ef262dac02b9e3d15
Gauthier Bastien <gauthier.bastien@imio.be>
2021-08-17 14:10:42 +0200

    Back to development: 1.0rc2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plonemeeting.restapi",
-    version="1.0rc1",
+    version="1.0rc2.dev0",
     description="Extended rest api service for Products.PloneMeeting usecases",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
