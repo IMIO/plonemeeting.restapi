7492782589bdb516c67930560e9707a6c00e4076
Gauthier Bastien <gauthier.bastien@imio.be>
2021-08-17 14:02:22 +0200

    Completed setup.py "Development Status" classifier to make checkmanifest happy

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
