e62dd82a7cce21fc31d5eec292066b170d08fcce
Gauthier Bastien <gauthier.bastien@imio.be>
2022-07-12 18:18:57 +0200

    Completed @config behavior to fix current tests, still need to add new behavior tests

--- src/plonemeeting/restapi/services/meetingconfig.py
+++ src/plonemeeting/restapi/services/meetingconfig.py
@@ -1,19 +1,42 @@
 # -*- coding: utf-8 -*-
 
-from plonemeeting.restapi.services.search import PMSearchGet
+from plone import api
+from plonemeeting.restapi.services.search import BaseSearchGet
+from plonemeeting.restapi.config import CONFIG_ID_NOT_FOUND_ERROR
+from zExceptions import BadRequest
 
 
-class ConfigSearchGet(PMSearchGet):
+class ConfigSearchGet(BaseSearchGet):
     """Returns a serialized content object.
     """
 
+    def __init__(self, context, request):
+        super(ConfigSearchGet, self).__init__(context, request)
+        # we may ask for every configs by using config_id=*
+        if self.config_id != "*":
+            self.tool = api.portal.get_tool("portal_plonemeeting")
+            self.cfg = self.tool.get(self.config_id, None)
+            if not self.cfg:
+                raise BadRequest(CONFIG_ID_NOT_FOUND_ERROR % self.config_id)
+
     @property
     def _type(self):
-        # forced to "config"
-        return "config"
+        # force to MeetingConfig
+        return "MeetingConfig"
+
+    def _set_query_additional_params(self):
+        """ """
+        query = super(ConfigSearchGet, self)._set_query_additional_params()
+        query.pop('getConfigId')
+        query['portal_type'] = self.type
+        if self.config_id != "*":
+            query['id'] = self.cfg.id
+        else:
+            query['sort_on'] = self.request.form.get('sort_on', 'getId')
+        return query
 
     def reply(self):
-        res = self._process_reply()
+        res = super(ConfigSearchGet, self).reply()
         if res["items"] and hasattr(self, "cfg"):
             # we asked for one single MeetingConfig, return it (not a list of result)
             res = res["items"][0]
