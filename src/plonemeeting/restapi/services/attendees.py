90b061db62efdb4056d314458f73082fb731fa0b
Gauthier Bastien <gauthier.bastien@imio.be>
2022-09-02 16:10:59 +0200

    Begin to implement PATCH for attendee

--- src/plonemeeting/restapi/services/attendees.py
+++ src/plonemeeting/restapi/services/attendees.py
@@ -9,6 +9,8 @@ from zope.interface import implementer
 from zope.interface import Interface
 from zope.publisher.interfaces import IPublishTraverse
 from zope.publisher.interfaces import NotFound
+from plone.restapi.deserializer import json_body
+from Products.PloneMeeting.utils import _itemNumber_to_storedItemNumber
 
 
 @implementer(IExpandableElement)
@@ -53,3 +55,45 @@ class AttendeeGet(Service):
     def reply(self):
         result = serialize_attendees(self.context, attendee_uid=self.attendee_uid)
         return result and result[0]
+
+
+@implementer(IPublishTraverse)
+class AttendeePatch(Service):
+
+    def __init__(self, context, request):
+        super(AttendeePatch, self).__init__(context, request)
+        self.attendee_uid = None
+
+    def publishTraverse(self, request, name):
+        if self.attendee_uid is None:
+            self.attendee_uid = name
+        else:
+            raise NotFound(self, name, request)
+        return self
+
+    def reply(self):
+        json = json_body(self.request)
+        is_meeting = self.context.__class__.__name__ == "Meeting"
+        meeting = self.context if is_meeting else self.context.getMeeting()
+        if is_meeting:
+            pass
+        else:
+            # MeetingItem
+            attendee_type_mappings = {'present': '@@item_welcome_attendee_form',
+                                      'absent': '@@item_byebye_attendee_form',
+                                      'excused': '@@item_byebye_attendee_form',
+                                      'non_attendee': '@@item_byebye_nonattendee_form'}
+
+            attendee_type = json.get('attendee_type')
+            form_name = attendee_type_mappings.get(attendee_type)
+            if not form_name:
+                raise Exception("Wrong attendee_type : \"%s\"" % attendee_type)
+            form = self.context.restrictedTraverse(form_name)
+            form.person_uid = self.attendee_uid
+            form.not_present_type = attendee_type
+            form.apply_until_item_number = _itemNumber_to_storedItemNumber(
+                json.get('until_item_number', u'0'))
+            form.meeting = meeting
+            form._doApply()
+            result = serialize_attendees(self.context, attendee_uid=self.attendee_uid)
+            return result and result[0]
