34e4403302bb741f39de33b583029020435d394f
Gauthier Bastien <gauthier.bastien@imio.be>
2024-05-06 12:52:16 +0200

    Added test_restapi_delete_meeting

--- src/plonemeeting/restapi/tests/test_services_delete.py
+++ src/plonemeeting/restapi/tests/test_services_delete.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from imio.helpers.content import object_values
 from plonemeeting.restapi.testing import PM_REST_TEST_ADD_PROFILE_FUNCTIONAL
 from plonemeeting.restapi.tests.base import BaseTestCase
 from Products.PloneMeeting.tests.PloneMeetingTestCase import DEFAULT_USER_PASSWORD
@@ -19,8 +20,8 @@ class testServiceDelete(BaseTestCase):
     def test_restapi_delete_item(self):
         """Delete an item."""
         self.changeUser("pmCreator1")
-        item = self.create('MeetingItem')
-        validated_item = self.create('MeetingItem')
+        item = self.create("MeetingItem")
+        validated_item = self.create("MeetingItem")
         self.validateItem(validated_item)
         transaction.commit()
         folder = item.getParentNode()
@@ -34,6 +35,47 @@ class testServiceDelete(BaseTestCase):
         transaction.commit()
         self.assertFalse(item in folder.objectValues())
         self.assertTrue(validated_item in folder.objectValues())
+
+    def test_restapi_delete_meeting(self):
+        """Delete a meeting."""
+        cfg = self.meetingConfig
+        self._removeConfigObjectsFor(cfg)
+        self.changeUser("pmManager")
+        empty_meeting = self.create("Meeting")
+        closed_meeting = self.create("Meeting")
+        self.closeMeeting(closed_meeting)
+        self.assertEqual(closed_meeting.query_state(), "closed")
+        meeting = self._createMeetingWithItems()
+        transaction.commit()
+        folder = empty_meeting.getParentNode()
+        self.assertTrue(empty_meeting in folder.objectValues())
+        # meeting with items
+        self.assertTrue(meeting in folder.objectValues())
+        self.assertTrue(object_values(folder, "MeetingItem"))
+        # trying to delete a meeting that is not empty will raise Unauthorized
+        response = self.api_session.delete(meeting.absolute_url())
+        self.assertEqual(response.status_code, 401, response.content)
+        # but deleting an empty meeting is ok
+        response = self.api_session.delete(empty_meeting.absolute_url())
+        self.assertEqual(response.status_code, 204, response.content)
+        # deleting a closed meeting will raise Unauthorized
+        response = self.api_session.delete(closed_meeting.absolute_url())
+        self.assertEqual(response.status_code, 401, response.content)
+        transaction.commit()
+        self.assertFalse(empty_meeting in folder.objectValues())
+        self.assertTrue(closed_meeting in folder.objectValues())
+        self.assertTrue(meeting in folder.objectValues())
+        self.assertTrue(object_values(folder, "MeetingItem"))
+        # a Manager may delete a meeting and it's items
+        self.api_session.auth = ("siteadmin", DEFAULT_USER_PASSWORD)
+        response = self.api_session.delete(meeting.absolute_url())
+        self.assertEqual(response.status_code, 204, response.content)
+        response = self.api_session.delete(closed_meeting.absolute_url())
+        self.assertEqual(response.status_code, 204, response.content)
+        transaction.commit()
+        self.assertFalse(closed_meeting in folder.objectValues())
+        self.assertFalse(meeting in folder.objectValues())
+        self.assertFalse(object_values(folder, "MeetingItem"))
 
 
 def test_suite():
